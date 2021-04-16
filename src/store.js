47484b2bccc6ca8ff342f7be383b92401aa05e8f
gevorgmanasyan2 <gevorgmanasyan2@gmail.com>
2021-04-16 19:47:04 +0400

    added new

--- src/store.js
+++ src/store.js
@@ -2,16 +2,40 @@ import {createStore,applyMiddleware,compose} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './reducer';
 import {sayHiOnDispatch, includeMeaningOfLife} from './exampleAddones/enhancers';
-import{print1,print2,print3} from './exampleAddones/middleware';
+import{print1,print2,print3,delayedActionMiddleware,fetchTodosMiddleware,asyncFunctionMiddleware} from './exampleAddones/middleware';
+import { client } from './api/client';
 
 // const composedEnhancer=compose(sayHiOnDispatch, includeMeaningOfLife)
 
 // const composedEnhancer=applyMiddleware(print1,print2,print3)
 
+const middlewareEnhancer=applyMiddleware(asyncFunctionMiddleware)
+
 const composedEnhancer=composeWithDevTools(
-    applyMiddleware(print1,print2,print3)
+    applyMiddleware(delayedActionMiddleware,fetchTodosMiddleware)
+    // applyMiddleware(print1,print2,print3)
     //other store enhancers if any
 )
-const store= createStore(rootReducer,composedEnhancer);
+// const store= createStore(rootReducer,composedEnhancer);
+const store= createStore(rootReducer,middlewareEnhancer);
+
+
+const fetchSomeData=(dispatch,getState)=>{
+    //Make an async HTTP request
+    client.get('todos').then(todos=>{
+        //Dispatch an action with the todos we received
+        dispatch({type:'todos/todosLoaded',payload:todos})
+        //Check the update store state after dispatching
+        const allTodos=getState().todos;
+        console.log("Number of todos after loading", allTodos.length);
+    })
+}
+
+//Pass the_function_ we wrote to `dispatch`
+store.dispatch(fetchSomeData)
+
+
+
+
 
 export default store;
